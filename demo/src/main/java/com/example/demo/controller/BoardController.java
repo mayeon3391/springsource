889ecd51b363a747a284fee94d1c14241ee1ceab
cheayeong <mase031477@gmail.com>
2025-04-16 18:01:44 +0900

    jpa 작성

--- demo/src/main/java/com/example/demo/controller/BoardController.java
+++ demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Log4j2
 @RequestMapping("/board")
@@ -15,6 +22,23 @@ public class BoardController {
     @GetMapping("/create")
     public void getCreate() {
         // return "board/create";
+    }
+
+    @PostMapping("/create")
+    // public String postCreate(@ModelAttribute("name") String name,
+    // RedirectAttributes rttr) {
+    public void postCreate(String name, HttpSession session) {
+        log.info("name 값 가져오기 {}", name);
+        session.setAttribute("name1", name);
+
+        // 어느 페이지로 이동을 하던지 간에 name 유지시키고 싶다면?
+        // 커맨드객체, ModelAttribute(or @ModelAttribute)
+        // return "board/list";
+
+        // redirect 값 유지하고 싶다면?
+        // rttr.addAttribute("name", name);
+        // rttr.addFlashAttribute("name", name);
+        // return "redirect:/board/list";
     }
 
     @GetMapping("/list")
